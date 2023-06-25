dee79734c90db06486191424851da90a6343ad64
yoon0811 <jhyeyun0811@gmail.com>
2023-06-24 23:18:53 -0700

    feat: add step 1

---
+++ js/script.js
@@ -0,0 +1,27 @@
+//// STEP 1 camel case
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction
+
+//// STEP 2 example of a numeric literal expression, a string, a Boolean, and a null.
+// let num = 3;
+// let string ='string';
+// let Boolean = true;
+// let null =null;  
+
+//// STEP 3 examples of complex / variable expressions
+// let a = 1 + 2 * (1 / 2);
+// const b = 3 + 4 / (3 * 2);
+// var c = a * b;
+
+
+
+//step 19. x라는 변수를 선언하고 값 20을 할당합니다.
+let x = 20;
+let multi = x*5;
+console.log(multi);
+
+let name = 'Hyeyoon Jeong'
+console.error(`Hello ${name}, welcome to the JavaScript class!`)
