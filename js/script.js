14b3ecbcdc22d08403be400304c4b0bbc77eb855
yoon0811 <jhyeyun0811@gmail.com>
2023-06-25 10:17:16 -0700

    feat: add STEP4 to 22

--- js/script.js
+++ js/script.js
@@ -5,15 +5,120 @@
 // let summerMonth;
 // let myLibraryFunction
 
-//// STEP 2 example of a numeric literal expression, a string, a Boolean, and a null.
+// STEP 2 example of a numeric literal expression, a string, a Boolean, and a null.
 // let num = 3;
 // let string ='string';
 // let Boolean = true;
-// let null =null;  
+// let value = null;  
 
 //// STEP 3 examples of complex / variable expressions
 // let a = 1 + 2 * (1 / 2);
 // const b = 3 + 4 / (3 * 2);
 // var c = a * b;
 
+//// STEP 4 Declare 9 variables 
+// let sFirstName;
+// let sLastName;
+// let sAddress, sCity, sState, iZipCode;
+// let iAge;
+// let referralSource;
+// let contact;
+
+//// STEP 5 demonstrate 3 ways for declaring and assigning values 
+// var sFirstName = 'Hyeyoon';
+// const sLastName = 'Jeong';
+// let iAge = 30;
+
+//// STEP 6 Create a variable
+// let user='Hyeyoon';
+// let age= 30;
+// console.log(user);
+// console.log(age);
+
+//// STEP 7 Create two variables
+// let boolean = true;
+// let a, b = (10, 20);
+// console.log(boolean);
+// console.log(a==b);
+
+// // STEP 8 Fix example 
+// let someString = `'Who once said, "Only two things are infinite, the universe and human stupidity, and I'm not sure about the former."'`;
+// window.console.log(someString);
+
+// // STEP 9 null 
+// let data = null;
+// console.log(data);
+// console.log(`${data='20'}`);
+
+// // STEP 10 
+// let a = '5'
+// console.log(typeof a);
+// let b = 5;
+// console.log(typeof b);
+// let c = true;
+// console.log(typeof c);
+// let d = null;  
+// console.log(typeof d);
+// let e;  
+// console.log(typeof e);
+
+// // STEP 11 alert box, use the concatenation operator (+) 
+// alert('Hello' + 'Zak Ruvalcaba,' + 'welcome to the JavaScript class!');
+
+// // STEP 12
+// let name;
+// name = 'Hyeyoon Jeong';
+// alert('Hello ' + `${name}, ` + 'welcome to the JavaScript class!');
+
+// // STEP 13
+// let sCourse;
+// sCourse = 'JavaScript';
+// alert('Hello ' + `${name}, ` + 'welcome to the' + `${sCourse}` + 'class!');
+
+// // STEP 14 
+// let name;
+// name = 'Hyeyoon Jeong';
+// let sCourse;
+// sCourse = 'JavaScript';
+// alert(`Hello ${name},\nwelcome to the ${sCourse} class!`);
+
+// // STEP 15
+// let name = prompt('What is your name?');
+// let sCourse;
+// sCourse = 'JavaScript';
+// alert(`Hello ${name},\nwelcome to the ${sCourse} class!`);
+
+// // STEP 16
+// let sName = prompt('What is your name?');
+// let scourse = prompt('What is your class?');
+// alert(`Hello ${sName},\nwelcome to the ${sCourse} class!`);
+
+// // STEP 17
+// let x = 10;
+// let y = 20;
+// console.log(x+y);
+
+// // STEP 18 
+// let x = 20;
+// console.log(x+20);
+
+// //STEP 19
+// let x = 20;
+// let multi = x*5;
+// console.log(multi);
+
+// // STEP 20
+// let x;
+// x = Math.floor(20 / 3);
+// console.log(x);
+
+// // STEP 21
+// let a = 5;
+// let b = 1 + 4;
+// console.log( a == b);
+
+// // STEP 22
+// let a = 5;
+// let b = (7 - 2);
+// console.log( a !== b);
 
